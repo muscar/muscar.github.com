f7be91cd20c451ee62e1ff1781be4c358ee27687
Alex Muscar <muscar@gmail.com>
2013-04-15 10:47:47 +0300

    Added AI.

---
+++ static/code/ai/rec_hw.pl
@@ -0,0 +1,30 @@
+% 1.) Write a predicate alt that given two lists produces a list of alternating
+% elements from the input lists, e.g.:
+%
+% ? alt([1, 2, 3], [4, 5, 6], L).
+% L = [1, 4, 2, 5, 3, 6].
+
+% 2.) Write a predicate that given a list and an element, adds the element at
+% the end of the input list, e.g.:
+%
+% ? add_right([1, 2, 3], 4, L).
+% L = [1, 2, 3, 4].
+
+% 3.) Write a predicate that given a list will produce all the prefixes of the
+% input list, e.g.:
+%
+% ? heads([1, 2, 3, 4], L).
+% L = [[], [1], [1, 2], [1, 2, 3]].
+
+
+% 4.) Write a predicate that given a list will produce all the suffixes of the
+% input list, e.g.:
+%
+% ? tails([1, 2, 3, 4], L).
+% L = [[2, 3, 4], [3, 4], [4], []].
+
+% 5.) Write a predicate that given two input lists produces a zipped list of
+% values from the input lists:
+%
+% ? zip([1, 2, 3], [4, 5, 6], L).
+% L = [pair(1, 4), pair(2, 5), pair(3, 6)].
