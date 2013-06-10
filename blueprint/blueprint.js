365f16e3d89e99a1955f7238b8ea7875b268f89c
Alex Muscar <muscar@gmail.com>
2013-06-10 11:55:17 +0300

    Updated Blueprint.

--- blueprint/blueprint.js
+++ blueprint/blueprint.js
@@ -1,48 +1,131 @@
-var idx = 0;
+function $variable(name) {
+	var self = this;
 
-function $term(tag) {
+	this.name = name;
+	this.value = null;
+
+	this.bound = function () {
+		return self.value != null;
+	};
+
+	this.bind = function (value) {
+		if (self.value != null) {
+			throw "Variable " + name + " is already bound.";
+		}
+		self.value = value;
+	};
+
+	this.unbind = function () {
+		self.value = null;
+	}
+
+	this.getValue = function () {
+		if (self.value instanceof $variable) {
+			return self.value.getValue();
+		}
+		return self.value;
+	}
+}
+
+$variable.prototype.toString = function () {
+	if (!this.bound()) {
+		return "#<" + this.name + ": unbound>";
+	}
+	return this.value.toString();
+}
+
+var $nil = {};
+
+function $cons(head, tail) {
+	var self = this;
+
+	this.head = head;
+	this.tail = tail;
+
+	this.getValue = function () {
+		var res = [];
+		var aux = self;
+		while (aux != $nil) {
+			res.push(aux.head);
+			aux = aux.tail;
+		}
+		return res;
+	}
+}
+
+$cons.ofArray = function (arr) {
+	return arr.reduceRight(function (head, tail) {
+		return new $cons(tail, head);
+	}, $nil);
+}
+
+function $term(tag, name) {
 	this.tag = tag;
-	this.idx = idx++;
-	for (var i = 1; i < arguments.length; i++)
+	this.name = name;
+
+	for (var i = 2; i < arguments.length; i++)
 	{
-		this["item" + i] = arguments[i]
+		this["item" + (i - 1)] = arguments[i]
 	}
 }
 
-$term.prototype.toString = function() {
-	if (this.item1 == undefined) {
-		return "_G" + this.idx;
-	}
-	var val = this.item1;
-	while (val instanceof $term && val.tag == -1) {
-		val = val.item1;
-	}
-	return val.toString();
+$term.prototype.toString = function () {
+	return this.name + "()";
 }
 
-function $unbind(term) {
-	term.item1 = undefined;
+function $atomic(term) {
+	var ty = typeof(term);
+	return ty == 'number' || ty == 'string' || term === $nil;
 }
 
 function $unify(left, right) {
-	if (left.tag == -1) {
-		if (left.item1 == undefined) {
-			left.item1 = right;
-			return true;
-		} else {
-			return $unify(left.item1, right);
-		}
-	} else if (right.tag == -1) {
-		if (right.item1 == undefined) {
-			right.item1 = left;
-			return true;
-		} else {
-			return $unify(left, right.item1);
-		}
-	} else if (typeof(left) === typeof(right)) {
-		return left === right;
+	if ($atomic(left) && $atomic(right)) {
+		return left == right;
 	}
+
+	if (left instanceof $cons && right instanceof $cons) {
+		return $unify(left.head, right.head) && $unify(left.tail, right.tail);
+	}
+
+	if (left instanceof $variable) {
+		if (!left.bound()) {
+			left.bind(right);
+			return true;
+		}
+		return $unify(left.getValue(), right);
+	}
+
+	if (right instanceof $variable) {
+		if (!right.bound()) {
+			right.bind(left);
+			return true;
+		}
+		return $unify(right.getValue(), left);
+	}
+
 	return false;
+}
+
+// Dictionary
+
+var blueprint = {
+	lang: {
+		dict: {
+			make: function () {
+				return {};
+			},
+			add: function (d, k, v) {
+				d[k] = v;
+			},
+			find: function (d, k) {
+				return d[k];
+			},
+			find_default: function (d, k, v) {
+				var val = d[k];
+				return val != undefined ? val : v;
+			}
+		}
+	}
 }
 
 // Promise lib
@@ -63,7 +146,7 @@ var promise = new promiseBuilder();
 
 // FB
 
-function FacebookClient() {
+function FB() {
 	this.get_wall_posts = function (v) {
 		var promise = jQuery.Deferred();
 		setTimeout(function () {
@@ -76,7 +159,7 @@ function FacebookClient() {
 
 // Twitter
 
-function TwitterClient() {
+function Twitter() {
 	this.get_tweets = function (v) {
 		var promise = jQuery.Deferred();
 		setTimeout(function () {
@@ -87,5 +170,5 @@ function TwitterClient() {
 	}
 }
 
-var facebook = new FacebookClient();
-var twitter = new TwitterClient();
+var facebook = new FB();
+var twitter = new Twitter();
